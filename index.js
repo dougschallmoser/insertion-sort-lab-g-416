7efba8ca32e0582d41d34115724f3c809fb5da57
Doug Schallmoser <dougschallmoser@gmail.com>
2020-12-07 01:53:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function insertionSort(array){
   
   let sorted = []
   while (array.length !== 0) {
-    let min = findMinAndRemove(array)
+    const min = findMinAndRemove(array)
     sorted.push(min)
   }
   return sorted
